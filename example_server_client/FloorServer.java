7b13587767badb5b08b7321967471d080dd83ee5
jaylenw <mr2cleanj@live.com>
2016-04-24 17:30:19 -0700

    added *class to gitignore and initialized server client test files

---
+++ example_server_client/FloorServer.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+
+public class FloorServer {
+
+    /*
+    *The port that the server will listen on
+    */
+
+    private static final int PORT = 8888;
+
+    public static void main(String[] args) throws IOException{
+      
+    }
+
+
+}
