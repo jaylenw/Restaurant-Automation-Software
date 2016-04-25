33416d069e52a25ac56eb2f4a91a8f92479e9d53
jaylenw <mr2cleanj@live.com>
2016-04-24 23:46:08 -0700

    modified

--- example_server_client/FloorServer.java
+++ example_server_client/FloorServer.java
@@ -16,7 +16,18 @@ public class FloorServer {
     private static final int PORT = 8888;
 
     public static void main(String[] args) throws IOException{
-      
+
+      ServerSocket server = new ServerSocket(PORT);
+
+      System.out.println("Waiting for clients to connect...");
+
+      while(true){
+
+        Socket s = server.accept();
+        System.out.println("A client connected.");
+
+      }
+
     }
 
 
