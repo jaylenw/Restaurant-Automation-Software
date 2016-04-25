bc865282c3a1c2c11aafc96b996d0617b88ecdfc
Michael <mbotsko7@gmail.com>
2016-04-24 17:05:24 -0700

    sql prepped for automating tables, ordering, and billing

--- RestaurantDatabase/RestaurantAutomation.sql
+++ RestaurantDatabase/RestaurantAutomation.sql
@@ -1,1 +1,24 @@
 --APACHE DERBY SQL FILE FOR RESTUARANT AUTOMATION SOFTWARE
+#works with customer stuff
+create table tables(
+	tableStatus varChar(1),
+	tableNumber INT
+	CONSTRAINT pk_tableNumber PRIMARY KEY (tableNumber);
+);
+
+create table menu(
+	itemName varChar (40),
+	price INT
+	CONSTRAINT pk_itemName PRIMARY KEY (itemName);
+);
+
+create table order(
+	orderID INT,
+	qty INT
+	CONSTRAINT fk_itemName FOREIGN KEY (itemName);
+);
+
+create table bill(
+	INT total
+	CONSTRAINT fk_tablesorder FOREIGN KEY (tableNumber, orderID);
+)
