c4c099541aa5bafbf6b8968f75acfe88e40394bf
Michael <mbotsko7@gmail.com>
2016-05-06 22:55:56 -0700

    update creation

--- RestaurantAutomation/src/restaurantautomation/RestrauntDatabase.java
+++ RestaurantAutomation/src/restaurantautomation/RestrauntDatabase.java
@@ -54,26 +54,38 @@ public class RestrauntDatabase {
     }
     
     public void build(){
+        PreparedStatement prep;
         try{
-       PreparedStatement prep = conn.prepareStatement("create table menu( "
+       prep = conn.prepareStatement("create table menu( "
                + "itemName varChar (20), price INT, CONSTRAINT pk_itemName "
                + "PRIMARY KEY (itemName))");
         prep.executeUpdate();
+        }
+        catch(SQLException sql){/*System.out.println(sql.getMessage());*/}
+        try{
         prep = conn.prepareStatement("create table employee("
                + "fname varChar (20), lname varChar (20), username varChar(20), "
                + "password varChar(20), priority INT;)");
-        prep.executeUpdate();
+        prep.executeUpdate();}
+        catch(SQLException sql){/*System.out.println(sql.getMessage());*/}
+        try{
         prep = conn.prepareStatement("create table tables( tableStatus varChar(1), "
                + "tableNumber INT, CONSTRAINT pk_tableNumber "
                + "PRIMARY KEY (tableNumber))");
-        prep.executeUpdate();
+        prep.executeUpdate();}
+        catch(SQLException sql){/*System.out.println(sql.getMessage());*/}
+        try{
         prep = conn.prepareStatement("create table orders( orderID INT, qty INT, "
                 + "itemName varChar(20), CONSTRAINT pk_orderID "
                 + "PRIMARY KEY (orderID), CONSTRAINT fk_itemName "
                 + "FOREIGN KEY (itemName) REFERENCES menu	(itemName))");
-        prep.executeUpdate();
+        prep.executeUpdate();}
+        catch(SQLException sql){/*System.out.println(sql.getMessage());*/}
+        try{
         prep = conn.prepareStatement("alter table orders add tableNumber int");
-        prep.executeUpdate();
+        prep.executeUpdate();}
+        catch(SQLException sql){/*System.out.println(sql.getMessage());*/}
+        try{
         prep = conn.prepareStatement("alter table orders add constraint "
                 + "fk_tableNum FOREIGN KEY (tableNumber) "
                 + "references tables(tableNumber)");
