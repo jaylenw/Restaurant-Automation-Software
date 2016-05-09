0be4ea95908a82579c9b6124e29f32af193eb907
jaylenw <mr2cleanj@live.com>
2016-05-08 19:42:21 -0700

    made adjustments and testing for table

--- Spark_Webview_Prototype/src/main/java/ChatWebSocketHandler.java
+++ Spark_Webview_Prototype/src/main/java/ChatWebSocketHandler.java
@@ -18,16 +18,16 @@ public class ChatWebSocketHandler {
 
     @OnWebSocketConnect
     public void onConnect(Session user) throws Exception {
-        String username = "User" + Chat.nextUserNumber++;
+        String username = "Employee " + Chat.nextUserNumber++;
         Chat.userUsernameMap.put(user, username);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
+        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the session"));
     }
 
     @OnWebSocketClose
     public void onClose(Session user, int statusCode, String reason) {
         String username = Chat.userUsernameMap.get(user);
         Chat.userUsernameMap.remove(user);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
+        Chat.broadcastMessage(sender = "Server", msg = (username + " left the session"));
     }
 
     @OnWebSocketMessage
